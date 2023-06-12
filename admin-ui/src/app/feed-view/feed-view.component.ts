f4d0ecc8e3615a726ca6c3c1b6c8433913ec5e2c
Marty Pitt <martypitt@gmail.com>
2023-06-12 18:21:05 +0100

    fix: render arrays

--- admin-ui/src/app/feed-view/feed-view.component.ts
+++ admin-ui/src/app/feed-view/feed-view.component.ts
@@ -11,73 +11,74 @@ import {TuiAlertService} from "@taiga-ui/core";
   selector: 'app-feed-view',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-      <app-header-component-layout [title]="feedSpec?.title!" [wide]="true">
-          <ng-container ngProjectAs="buttons">
-              <button tuiButton appearance="outline" size="m" (click)="executeFeed()" [showLoader]="executing">Run now
-              </button>
-          </ng-container>
-          <ng-container ngProjectAs="header-components">
-              <span>Rss feed from <a [href]="rssConfig?.url" target="_blank">{{rssConfig?.url}}</a></span>
-              <div class="mt-8">
-                  <tui-accordion-item class="container">
-                      <div class="text-sm">API available at <span
-                              class="font-mono">{{hydrateUrl(feedSpec?.apiEndpoint)}}</span></div>
-                      <div class="text-xs">Expand for more API details</div>
-                      <ng-template tuiAccordionItemContent>
-                          <table>
-                              <tr *ngFor="let endpoint of apiEndpoints">
-                                  <td class="text-sm pr-8">{{endpoint.label}}</td>
-                                  <td class="text-sm">
-                                      <div class="flex items-center">
-                                        {{ endpoint.value }}
-                                        <tui-svg class="ml-4 cursor-pointer text-gray-400 group-hover:text-indigo-600" src="tuiIconCopy" (click)="copyToClipboard(endpoint.value)"></tui-svg>
-                                      </div>
-                                  </td>
-                              </tr>
-                          </table>
-                      </ng-template>
-                  </tui-accordion-item>
-              </div>
-          </ng-container>
-
-          <!--        <tui-scrollbar-->
-          <!--          waIntersectionRoot-->
-          <!--          class="scrollbar"-->
-          <!--          [hidden]="true"-->
-          <!--        >-->
-          <div class="overflow-scroll">
-              <table tuiTable class="table" style="border-collapse: collapse"
-                     *ngIf="feedSpec" [columns]="columns">
-                  <thead>
-                  <tr tuiThGroup>
-                      <th tuiTh>Signal type</th>
-                      <th tuiTh>Signal URI</th>
-                      <th tuiTh>Result</th>
-                      <th tuiTh>Error message</th>
-                      <th tuiTh *ngFor="let col of feedSpec.extractions" [resizable]="true">{{ col.title }}</th>
-                  </tr>
-                  </thead>
-                  <tbody tuiTbody [data]="feedData">
-                  <tr *ngFor="let row of feedData" tuiTr>
-                      <td *tuiCell="'signalType'">{{row.source.id.feedType}}</td>
-                      <td *tuiCell="'uri'"><a [attr.href]="row.source.signalUri"
-                                              target="_blank">{{row.source.signalUri}}</a></td>
-                      <td *tuiCell="'success'">{{ row.success}}</td>
-                      <td *tuiCell="'errorMessage'">{{ row.errorMessage}}</td>
-                      <ng-container *ngFor="let col of feedSpec.extractions">
-                          <td *tuiCell="col.title">
-                              <ng-container *ngIf="row.success">
-                                  {{ row.signals[col.title]}}
-                              </ng-container>
-                          </td>
-                      </ng-container>
-
-                  </tr>
-
-                  </tbody>
+    <app-header-component-layout [title]="feedSpec?.title!" [wide]="true">
+      <ng-container ngProjectAs="buttons">
+        <button tuiButton appearance="outline" size="m" (click)="executeFeed()" [showLoader]="executing">Run now
+        </button>
+      </ng-container>
+      <ng-container ngProjectAs="header-components">
+        <span>Rss feed from <a [href]="rssConfig?.url" target="_blank">{{rssConfig?.url}}</a></span>
+        <div class="mt-8">
+          <tui-accordion-item class="container">
+            <div class="text-sm">API available at <span
+              class="font-mono">{{hydrateUrl(feedSpec?.apiEndpoint)}}</span></div>
+            <div class="text-xs">Expand for more API details</div>
+            <ng-template tuiAccordionItemContent>
+              <table>
+                <tr *ngFor="let endpoint of apiEndpoints">
+                  <td class="text-sm pr-8">{{endpoint.label}}</td>
+                  <td class="text-sm">
+                    <div class="flex items-center">
+                      {{ endpoint.value }}
+                      <tui-svg class="ml-4 cursor-pointer text-gray-400 group-hover:text-indigo-600" src="tuiIconCopy"
+                               (click)="copyToClipboard(endpoint.value)"></tui-svg>
+                    </div>
+                  </td>
+                </tr>
               </table>
-          </div>
-      </app-header-component-layout>
+            </ng-template>
+          </tui-accordion-item>
+        </div>
+      </ng-container>
+
+      <!--        <tui-scrollbar-->
+      <!--          waIntersectionRoot-->
+      <!--          class="scrollbar"-->
+      <!--          [hidden]="true"-->
+      <!--        >-->
+      <div class="overflow-scroll">
+        <table tuiTable class="table" style="border-collapse: collapse"
+               *ngIf="feedSpec" [columns]="columns">
+          <thead>
+          <tr tuiThGroup>
+            <th tuiTh>Signal type</th>
+            <th tuiTh>Signal URI</th>
+            <th tuiTh>Result</th>
+            <th tuiTh>Error message</th>
+            <th tuiTh *ngFor="let col of feedSpec.extractions" [resizable]="true">{{ col.title }}</th>
+          </tr>
+          </thead>
+          <tbody tuiTbody [data]="feedData">
+          <tr *ngFor="let row of feedData" tuiTr>
+            <td *tuiCell="'signalType'">{{row.source.id.feedType}}</td>
+            <td *tuiCell="'uri'"><a [attr.href]="row.source.signalUri"
+                                    target="_blank">{{row.source.signalUri}}</a></td>
+            <td *tuiCell="'success'">{{ row.success}}</td>
+            <td *tuiCell="'errorMessage'">{{ row.errorMessage}}</td>
+            <ng-container *ngFor="let col of feedSpec.extractions">
+              <td *tuiCell="col.title">
+                <ng-container *ngIf="row.success">
+                  {{ unwrapArrays(row.signals[col.title])}}
+                </ng-container>
+              </td>
+            </ng-container>
+
+          </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </app-header-component-layout>
   `,
   styleUrls: ['./feed-view.component.scss']
 })
@@ -91,14 +92,14 @@ export class FeedViewComponent {
   feedData: SignalParseResult[] = [];
   columns: string[] = [];
 
-  get rssConfig():RssConfig | null {
+  get rssConfig(): RssConfig | null {
     if (this.feedSpec?.type !== "RssFeed") {
       return null;
     }
     return this.feedSpec?.config as RssConfig
   }
 
-  get apiEndpoints():{ label: string, value: string }[] {
+  get apiEndpoints(): { label: string, value: string }[] {
     if (this.feedSpec === null) {
       return [];
     }
@@ -113,7 +114,7 @@ export class FeedViewComponent {
               private feedService: FeedsService,
               private changeDetectorRef: ChangeDetectorRef,
               private alertService: TuiAlertService
-              ) {
+  ) {
     activeRoute.paramMap
       .pipe(mergeMap(params => {
         const id = params.get('id');
@@ -155,6 +156,7 @@ export class FeedViewComponent {
     }
     return `${environment.serverUrl}${apiEndpoint}`
   }
+
   async copyToClipboard(text: string | null) {
     if (text === null) {
       return
@@ -168,6 +170,20 @@ export class FeedViewComponent {
     } catch (err) {
       this.alertService.open('Something went wrong copying to the clipboard').subscribe()
       console.log('Failed to copy text: ', err);
+    }
+  }
+
+  unwrapArrays(signal: any) {
+    if (signal === null) {
+      return "";
+    }
+    if (signal === undefined) {
+      return "";
+    }
+    if (typeof signal !== 'object') {
+      return signal;
+    } else {
+      return JSON.stringify(signal, null, 3)
     }
   }
 }
